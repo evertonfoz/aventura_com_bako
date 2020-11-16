9c4b23ebe43391260cfb419fab2f26af5b9278a8
rodrigorafaeldamaceno <rodrigorafaeldamaceno@gmail.com>
2020-11-16 16:17:21 -0300

    splashscreen adjustments

--- lib/screens/splash/splash.dart
+++ lib/screens/splash/splash.dart
@@ -1,46 +1,84 @@
+import 'package:aventura_com_bako/screens/home/home_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  Size size;
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(Duration(seconds: 2)).then(
+      (value) {
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(
+            builder: (context) => HomeScreen(),
+          ),
+          (route) => false,
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          body: Stack(
+    size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Stack(
         children: <Widget>[
           Container(
+            padding: EdgeInsets.only(bottom: 10),
             decoration: new BoxDecoration(
               image: new DecorationImage(
-                  image: new ExactAssetImage('assets/background-splash.jpg'),
-                  fit: BoxFit.cover),
+                image: new ExactAssetImage('assets/background-splash.jpg'),
+                fit: BoxFit.cover,
+              ),
             ),
             child: Align(
               alignment: Alignment.bottomCenter,
               child: Text(
                 'Desenvolvido pelo time da UTFPR',
                 style: TextStyle(
-                    fontFamily: 'Arial',
-                    fontSize: 20,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold),
+                  fontFamily: 'Arial',
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
                 textAlign: TextAlign.center,
                 textDirection: TextDirection.ltr,
               ),
             ),
           ),
           Container(
-              margin: const EdgeInsets.only(bottom: 50.0, left: 70.0, right: 70.0),
-              alignment: Alignment.bottomCenter,
-              child: Image.asset(
-                'assets/bako_vetor.png',
-                fit: BoxFit.fitWidth,
-              )),
+            margin: const EdgeInsets.only(bottom: 50.0),
+            alignment: Alignment.bottomCenter,
+            child: Image.asset(
+              'assets/bako_vetor.png',
+              fit: BoxFit.fitWidth,
+              height: size.width * 0.8,
+            ),
+          ),
           Container(
             alignment: Alignment.topCenter,
-            margin: const EdgeInsets.only(left: 40.0, top: 110.0, right: 40.0),
-            child: Image.asset('assets/idv.png'),
+            margin: EdgeInsets.only(
+              // left: 40.0,
+              top: size.height * 0.1,
+              // right: 40.0,
+            ),
+            child: Image.asset(
+              'assets/idv.png',
+              width: size.width * 0.7,
+            ),
           )
         ],
-      )),
+      ),
     );
   }
 }
