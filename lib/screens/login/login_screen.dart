e36c1c66d27260080ff75d53fef27ab404f25a09
rodrigorafaeldamaceno <rodrigorafaeldamaceno@gmail.com>
2020-12-08 18:37:32 -0300

    some adjustments

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -8,88 +8,126 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool reminderPass = false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Stack(
-        children: <Widget>[
-          Opacity(
-            opacity: 0.3,
-            child: Container(
-              height: double.infinity,
-              width: double.infinity,
-              decoration: new BoxDecoration(
-                image: new DecorationImage(
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).requestFocus(FocusNode());
+      },
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Stack(
+          children: <Widget>[
+            Opacity(
+              opacity: 0.3,
+              child: Container(
+                height: double.infinity,
+                width: double.infinity,
+                decoration: new BoxDecoration(
+                  image: new DecorationImage(
                     image: new ExactAssetImage(
                       'assets/background-splash.jpg',
                     ),
-                    fit: BoxFit.cover),
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
             ),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Center(
+            ListView(
+              // crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Center(
                   child: Container(
-                      padding: EdgeInsets.symmetric(vertical: 75.0),
-                      child: new Image.asset(
-                        'assets/idv.png',
-                        height: 80.0,
-                        fit: BoxFit.cover,
-                      ))),
-              Container(
-                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                alignment: Alignment.centerLeft,
-                child: TextFormField(
-                  decoration: InputDecoration(
-                      hintText: "Usuário ou E-mail",
-                      enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey)),
-                      border: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey)),
-                      focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey))),
-                  autocorrect: false,
-                  keyboardType: TextInputType.emailAddress,
-                  cursorColor: Colors.green,
-                  style: TextStyle(fontSize: 18),
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                alignment: Alignment.centerLeft,
-                height: 60.0,
-                child: TextField(
-                  decoration: InputDecoration(
-                      hintText: "Senha",
-                      enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey)),
-                      border: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey)),
-                      focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey))),
-                  obscureText: true,
-                  enableSuggestions: false,
-                  autocorrect: false,
-                  keyboardType: TextInputType.text,
-                  cursorColor: Colors.green,
-                  style: TextStyle(fontSize: 18),
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 5.0),
-                child: CheckboxListTile(
-                  title: Text(
-                    "Lembrar de mim",
-                    style: TextStyle(fontSize: 18, color: Colors.black54),
+                    child: new Image.asset(
+                      'assets/idv.png',
+                      height: 80.0,
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                  value: true,
-                  onChanged: (value) {},
                 ),
-              ),
-              Container(
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  padding:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  alignment: Alignment.centerLeft,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Usuário ou E-mail",
+                        fillColor: Colors.white.withOpacity(0.7),
+                        filled: true,
+                        border: InputBorder.none
+                        // enabledBorder: UnderlineInputBorder(
+                        //   borderSide: BorderSide(color: Colors.grey),
+                        // ),
+                        // border: UnderlineInputBorder(
+                        //   borderSide: BorderSide(color: Colors.grey),
+                        // ),
+                        // focusedBorder: UnderlineInputBorder(
+                        //   borderSide: BorderSide(color: Colors.grey),
+                        // ),
+                        ),
+                    autocorrect: false,
+                    keyboardType: TextInputType.emailAddress,
+                    cursorColor: Colors.green,
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+                Container(
+                  padding:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  // alignment: Alignment.centerLeft,
+                  // height: 60.0,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Senha",
+                      fillColor: Colors.white.withOpacity(0.7),
+                      filled: true,
+                      border: InputBorder.none,
+
+                      // enabledBorder: UnderlineInputBorder(
+                      //   borderSide: BorderSide(color: Colors.grey),
+                      // ),
+                      // border: UnderlineInputBorder(
+                      //   borderSide: BorderSide(color: Colors.grey),
+                      // ),
+                      // focusedBorder: UnderlineInputBorder(
+                      //   borderSide: BorderSide(color: Colors.grey),
+                      // ),
+                    ),
+                    obscureText: true,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    keyboardType: TextInputType.text,
+                    cursorColor: Colors.green,
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 5.0),
+                  child: CheckboxListTile(
+                    title: Text(
+                      "Lembrar de mim?",
+                      style: TextStyle(fontSize: 18, color: Colors.black54),
+                    ),
+                    value: reminderPass,
+                    onChanged: (value) {
+                      setState(() {
+                        reminderPass = value;
+                      });
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
                   width: double.infinity,
                   height: 90.0,
                   padding:
@@ -98,67 +136,78 @@ class _LoginScreenState extends State<LoginScreen> {
                     onPressed: () {},
                     textColor: Colors.white,
                     color: Colors.green,
-                    child: const Text('ENTRAR',
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.normal)),
-                  )),
-              Container(
-                width: double.infinity,
-                alignment: Alignment.center,
-                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
-                child: Text(
-                  'ou',
-                  style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.normal,
-                      color: Colors.black54),
-                ),
-              ),
-              Container(
-                width: double.infinity,
-                height: 65,
-                padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
-                child: SignInButton(
-                  Buttons.Facebook,
-                  text: "ENTRAR COM FACEBOOK",
-                  onPressed: () {},
-                ),
-              ),
-              Container(
-                width: double.infinity,
-                height: 65,
-                padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
-                child: SignInButton(
-                  Buttons.Google,
-                  text: "ENTRAR COM GOOGLE",
-                  onPressed: () {},
-                ),
-              ),
-              Container(
-                alignment: Alignment.center,
-                width: double.infinity,
-                padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
-                child: InkWell(
-                  child: new RichText(
-                    text: new TextSpan(
-                      style: new TextStyle(
-                        fontSize: 18.0,
-                        color: Colors.black,
+                    child: const Text(
+                      'ENTRAR',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.normal,
                       ),
-                      children: <TextSpan>[
-                        new TextSpan(text: 'Não tem uma conta? '),
-                        new TextSpan(
-                            text: 'Registre-se',
-                            style: new TextStyle(fontWeight: FontWeight.bold)),
-                      ],
                     ),
                   ),
-                  onTap: () {},
                 ),
-              )
-            ],
-          )
-        ],
+                Container(
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  // padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+                  child: Text(
+                    'ou',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.black54,
+                    ),
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 65,
+                  padding:
+                      EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
+                  child: SignInButton(
+                    Buttons.Facebook,
+                    text: "ENTRAR COM FACEBOOK",
+                    onPressed: () {},
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 65,
+                  padding:
+                      EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
+                  child: SignInButton(
+                    Buttons.Google,
+                    text: "ENTRAR COM GOOGLE",
+                    onPressed: () {},
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  width: double.infinity,
+                  padding:
+                      EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
+                  child: InkWell(
+                    child: new RichText(
+                      text: new TextSpan(
+                        style: new TextStyle(
+                          fontSize: 18.0,
+                          color: Colors.black,
+                        ),
+                        children: <TextSpan>[
+                          new TextSpan(text: 'Não tem uma conta? '),
+                          new TextSpan(
+                            text: 'Registre-se',
+                            style: new TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                    onTap: () {},
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
