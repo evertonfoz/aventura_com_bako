b1c15c4c13a0c439bf6645d8e55627e7d7776c35
Nathã Luca Pereira Lopes <natha.luca.p.lopes@outlook.com>
2021-02-08 18:29:10 -0300

    Desabilitando a rotação de tela

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:aventura_com_bako/core/routes.dart';
 import 'package:aventura_com_bako/features/home/presentation/pages/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,6 +12,10 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+
     return LayoutBuilder(builder: (context, constraints) {
       ScreenUtil.init(constraints,
           designSize: Size(393, 813), allowFontScaling: false);
