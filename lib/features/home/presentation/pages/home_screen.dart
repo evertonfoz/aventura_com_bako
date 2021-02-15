d6a7c9c15deeafff1cb252f32d5c171c0e9db8fa
Nathã Luca Pereira Lopes <natha.luca.p.lopes@outlook.com>
2021-02-15 18:52:04 -0300

    Adicionando a foto de perfil ao card

--- lib/features/home/presentation/pages/home_screen.dart
+++ lib/features/home/presentation/pages/home_screen.dart
@@ -31,39 +31,76 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
-      body: Stack(
-        children: [
-          Container(
-            height: double.infinity,
-            width: double.infinity,
-            child: Image.asset(
-              'assets/background_forest.jpg',
-              fit: BoxFit.cover,
-            ),
+      body: HomePageBody(),
+    );
+  }
+}
+
+class HomePageBody extends StatelessWidget {
+  final double circleRadius = 100.0;
+  final double circleBorderWidth = 8.0;
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          height: double.infinity,
+          width: double.infinity,
+          child: Image.asset(
+            'assets/background_forest.jpg',
+            fit: BoxFit.cover,
           ),
-          Center(
-            child: Card(
-              child: SizedBox(
-                height: 450,
-                child: Column(
-                  children: [
-                    Container(
-                      width: 370,
-                      height: 70,
-                      color: Colors.white,
+        ),
+        Center(
+          child: Stack(
+            alignment: Alignment.topCenter,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(top: circleRadius / 2.0),
+                child: Card(
+                  child: SizedBox(
+                    height: 450,
+                    child: Column(
+                      children: [
+                        Container(
+                          width: 370,
+                          height: 70,
+                          color: Colors.white,
+                        ),
+                        Container(
+                          width: 370,
+                          height: 380,
+                          color: Colors.green,
+                        )
+                      ],
                     ),
-                    Container(
-                      width: 370,
-                      height: 380,
-                      color: Colors.green,
-                    )
-                  ],
+                  ),
                 ),
               ),
-            ),
-          )
-        ],
-      ),
+              Container(
+                width: circleRadius,
+                height: circleRadius,
+                decoration:
+                    ShapeDecoration(shape: CircleBorder(), color: Colors.white),
+                child: Padding(
+                  padding: EdgeInsets.all(circleBorderWidth),
+                  child: DecoratedBox(
+                    decoration: ShapeDecoration(
+                      shape: CircleBorder(),
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                          'https://upload.wikimedia.org/wikipedia/commons/a/a0/Bill_Gates_2018.jpg',
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
