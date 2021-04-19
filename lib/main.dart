97a601545f51964b1d7ee8da3c6579e22964ecf5
GabrielRStabile <gabrielrostabile@outlook.com>
2021-04-19 13:17:54 -0300

    :fire: Removido lógica do provider
    Agora quem cuidará de saber se o usuário está ou não logado vai ser a SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:aventura_com_bako/core/routes.dart';
-import 'package:aventura_com_bako/features/login/data/repositories/firebase_auth_repository.dart';
 import 'package:aventura_com_bako/features/splash/presentation/pages/splash_screen.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:provider/provider.dart';
+import 'package:get/get.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,30 +14,22 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        Provider<FireAuth>(
-          create: (_) => FireAuth(FirebaseAuth.instance),
+    return LayoutBuilder(builder: (context, constraints) {
+      ScreenUtil.init(constraints,
+          designSize: Size(393, 813), allowFontScaling: false);
+      return GetMaterialApp(
+        title: 'Aventura com Bako',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          fontFamily: 'Nunito',
         ),
-        StreamProvider(create: (context) => context.read<FireAuth>().authState)
-      ],
-      child: LayoutBuilder(builder: (context, constraints) {
-        ScreenUtil.init(constraints,
-            designSize: Size(393, 813), allowFontScaling: false);
-        return MaterialApp(
-          title: 'Aventura com Bako',
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            primarySwatch: Colors.green,
-            visualDensity: VisualDensity.adaptivePlatformDensity,
-            fontFamily: 'Nunito',
-          ),
-          onGenerateRoute: Routes.generateRoute,
-          navigatorKey: Routes.navigatorKey,
-          initialRoute: Routes.splashScreen,
-          home: SplashScreen(),
-        );
-      }),
-    );
+        onGenerateRoute: Routes.generateRoute,
+        navigatorKey: Routes.navigatorKey,
+        initialRoute: Routes.splashScreen,
+        home: SplashScreen(),
+      );
+    });
   }
 }
