0709e04c10f8a34fa129b9b2b482222eec6d5576
GabrielRStabile <gabrielrostabile@outlook.com>
2021-04-19 13:11:53 -0300

    :truck: Rota removida
    Agora a verificação se um usuário está ou não logado é feita pela SplashScreen

--- lib/core/routes.dart
+++ lib/core/routes.dart
@@ -1,6 +1,6 @@
 import 'package:aventura_com_bako/features/home/presentation/pages/home_screen.dart';
 import 'package:aventura_com_bako/features/login/presentation/pages/login_page.dart';
-import 'package:aventura_com_bako/features/splash/domain/usecases/authenticate.dart';
+import 'package:aventura_com_bako/features/login/presentation/pages/signup_page.dart';
 import 'package:aventura_com_bako/features/splash/presentation/pages/splash_screen.dart';
 import 'package:aventura_com_bako/features/tutorial/presentation/pages/tutorial_screen.dart';
 import 'package:flutter/material.dart';
@@ -12,7 +12,7 @@ class Routes {
   static const String login = '/login';
   static const String tutorial = '/tutorial';
   static const String home = '/home';
-  static const String auth = '/auth';
+  static const String signup = '/signup';
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     final args = settings.arguments;
@@ -31,8 +31,8 @@ class Routes {
       case tutorial:
         return MaterialPageRoute(builder: (_) => TutorialPage());
         break;
-      case auth:
-        return MaterialPageRoute(builder: (_) => Authenticate());
+      case signup:
+        return MaterialPageRoute(builder: (_) => SignUpScreen());
         break;
       default:
         _errorRoute();
