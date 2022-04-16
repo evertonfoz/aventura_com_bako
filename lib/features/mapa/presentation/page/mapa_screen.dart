f44bfeb7a2423367fcd183f31b68dcfc7e2f9a5a
Barbara Rodrigues <barbara.rodrigues@fmxsolucoes.com.br>
2022-04-16 00:20:02 -0300

    [Barbara R] criação mapa digital e tiled

--- lib/features/mapa/presentation/page/mapa_screen.dart
+++ lib/features/mapa/presentation/page/mapa_screen.dart
@@ -4,6 +4,7 @@ import 'package:aventura_com_bako/features/mapa/helpers/enums/direction_enum.dar
 import 'package:aventura_com_bako/features/mapa/widgets/bako_widget.dart';
 import 'package:aventura_com_bako/features/mapa/widgets/mapa_widget.dart';
 import 'package:flame/game.dart';
+import 'package:flame_tiled/flame_tiled.dart';
 
 class MapScreen extends FlameGame {
   final Bako _bako = Bako();
@@ -11,10 +12,10 @@ class MapScreen extends FlameGame {
 
   @override
   Future<void> onLoad() async {
-    await add(_mapa);
+    add(_mapa);
     add(_bako);
 
-    _bako.position = _mapa.size / 2;
+    _bako.position = _mapa.size / 8;
     camera.followComponent(_bako,
         worldBounds: Rect.fromLTRB(0, 0, _mapa.size.x, _mapa.size.y));
   }
