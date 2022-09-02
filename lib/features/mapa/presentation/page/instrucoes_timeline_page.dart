da404d97fd42a7da10d5d49a7f9e009d794e09b4
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2022-09-02 11:31:16 -0300

    Ajustes

--- lib/features/mapa/presentation/page/instrucoes_timeline_page.dart
+++ lib/features/mapa/presentation/page/instrucoes_timeline_page.dart
@@ -1,9 +1,14 @@
-import 'package:aventura_com_bako/features/mapa/presentation/page/mapa_main_page.dart';
 import 'package:basic_utils/basic_utils.dart';
+import 'package:eca_packages/eca_packages.dart';
 import 'package:flutter/material.dart';
 import 'package:timelines/timelines.dart';
 
+import 'mapa_main_page.dart';
+import 'welcome_page.dart';
+
 class InstrucoesTimelinePage extends StatefulWidget {
+  const InstrucoesTimelinePage({Key? key}) : super(key: key);
+
   @override
   State<InstrucoesTimelinePage> createState() => _InstrucoesTimelinePageState();
 }
@@ -12,159 +17,163 @@ class _InstrucoesTimelinePageState extends State<InstrucoesTimelinePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.maxFinite,
-        height: double.maxFinite,
-        decoration: new BoxDecoration(
-          image: new DecorationImage(
-            image: ExactAssetImage('assets/Padrão4.jpg'),
-            fit: BoxFit.fitHeight,
-          ),
-          color: Color(
-            ColorUtils.hexToInt("#94BF36"),
+        body: Stack(
+      children: [
+        Opacity(
+          opacity: 0.5,
+          child: Container(
+            width: double.maxFinite,
+            height: double.maxFinite,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: ExactAssetImage('assets/Padrão4.jpg'), //TODO Constantes
+                fit: BoxFit.fitHeight,
+              ),
+            ),
           ),
         ),
-        child: Padding(
-          padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
+        Align(
+          alignment: Alignment.topCenter,
+          child: Container(
+            width: 300.00,
+            height: 150.00,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: ExactAssetImage('assets/idv.png'), //TODO Constantes
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 160.0, left: 10),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.max,
+            // mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Center(
-                child: Container(
-                  width: 200.00,
-                  height: 100.00,
-                  decoration: new BoxDecoration(
-                    image: new DecorationImage(
-                      image: ExactAssetImage('assets/idv.png'),
-                      fit: BoxFit.contain,
-                    ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.85,
+                height: 120,
+                decoration: ShapeDecoration(
+                  color: Colors.yellow,
+                  shape: TooltipShapeBorder(
+                    arrowArc: 0.5,
+                    arrowHeight: 35,
+                  ),
+                  shadows: const [
+                    BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 4.0,
+                        offset: Offset(2, 2))
+                  ],
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      TextECA(
+                        text: 'Sua aventura está prestes a começar',
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      const SizedBox(height: 10),
+                      TextECA(
+                        text: 'Mas antes, vamos à algumas orientações',
+                        fontSize: 16,
+                      ),
+                    ],
                   ),
                 ),
               ),
               Container(
-                  padding: EdgeInsets.all(12),
-                  decoration: BoxDecoration(
+                width: 200.00,
+                height: 200.00,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: ExactAssetImage(
+                        'assets/bako_vetor.png'), //TODO Constantes
+                    fit: BoxFit.fitHeight,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 480.0, left: 140),
+          child: Timeline.tileBuilder(
+            theme: TimelineThemeData(
+              nodePosition: 0,
+              indicatorTheme: IndicatorThemeData(
+                size: 20,
+                color: Color(
+                  ColorUtils.hexToInt("#FEE784"),
+                ),
+              ),
+              connectorTheme: ConnectorThemeData(
+                color: Color(
+                  ColorUtils.hexToInt("#FEE784"),
+                ),
+                space: 30.0,
+                thickness: 5.0,
+                indent: 0,
+              ),
+            ),
+            builder: TimelineTileBuilder.fromStyle(
+              contentsAlign: ContentsAlign.basic,
+              contentsBuilder: (context, index) => Padding(
+                padding: const EdgeInsets.only(top: 25, bottom: 25, left: 20),
+                child: Container(
+                  padding: const EdgeInsets.all(12),
+                  decoration: const BoxDecoration(
                     borderRadius: BorderRadius.all(
                       Radius.circular(10.0),
                     ),
                     color: Colors.white60,
                   ),
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        'Sua aventura está prestes a começar...',
-                        textAlign: TextAlign.start,
-                        style: TextStyle(
-                          color: Colors.green[900],
-                          fontSize: 24,
-                        ),
-                      ),
-                      Text(
-                        'Mas antes, algumas instruções!',
-                        textAlign: TextAlign.start,
-                        style: TextStyle(
-                          color: Colors.green[900],
-                          fontSize: 20,
+                        '${textosDeInformacao[index]}',
+                        style: const TextStyle(
+                          color: Colors.black54,
+                          fontSize: 15,
                         ),
                       ),
                     ],
-                  )),
-              SizedBox(height: 25),
-              SizedBox(
-                height: 400,
-                child: Timeline.tileBuilder(
-                  theme: TimelineThemeData(
-                    nodePosition: 0,
-                    indicatorTheme: IndicatorThemeData(
-                      size: 20,
-                      color: Color(
-                        ColorUtils.hexToInt("#FEE784"),
-                      ),
-                    ),
-                    connectorTheme: ConnectorThemeData(
-                      color: Color(
-                        ColorUtils.hexToInt("#FEE784"),
-                      ),
-                      space: 30.0,
-                      thickness: 5.0,
-                      indent: 0,
-                    ),
-                  ),
-                  builder: TimelineTileBuilder.fromStyle(
-                    contentsAlign: ContentsAlign.basic,
-                    contentsBuilder: (context, index) => Padding(
-                      padding:
-                          const EdgeInsets.only(top: 25, bottom: 25, left: 20),
-                      child: Container(
-                        padding: EdgeInsets.all(12),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(10.0),
-                          ),
-                          color: Colors.white60,
-                        ),
-                        child: Column(
-                          children: [
-                            Text(
-                              '${textosDeInformacao[index]}',
-                              style: TextStyle(
-                                color: Colors.black54,
-                                fontSize: 15,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    itemCount: textosDeInformacao.length,
                   ),
                 ),
               ),
-              Center(
-                child: Container(
-                  padding: EdgeInsets.only(bottom: 50),
-                  width: 220,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => MapPage(),
-                        ),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      shape: new RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(30.0),
-                      ),
-                      primary: Color(
-                        ColorUtils.hexToInt("#FEE784"),
-                      ),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(13.0),
-                      child: Text(
-                        'Iniciar a trilha',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w800,
-                          color: Color(
-                            ColorUtils.hexToInt("#788A25"),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              )
-            ],
+              itemCount: textosDeInformacao.length,
+            ),
           ),
         ),
-      ),
-    );
+        Align(
+          alignment: Alignment.bottomRight,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SizedBox(
+              width: 80,
+              height: 80,
+              child: FloatingActionButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const MapPage(),
+                    ),
+                  );
+                },
+                child: const Icon(
+                  Icons.arrow_forward,
+                  size: 60,
+                  // color: kBrandColor,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    ));
   }
 
   var textosDeInformacao = [
