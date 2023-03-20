c8f6454791b336129e199abfc9fb438861a787fb
Sanders00 <96478702+Sanders00@users.noreply.github.com>
2023-03-20 13:35:55 -0300

    Desenvolvimento da galeria

--- lib/features/galeria/presentation/pages/galeria_screen.dart
+++ lib/features/galeria/presentation/pages/galeria_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:basic_utils/basic_utils.dart';
-import 'package:eca_packages/eca_packages.dart';
+import 'package:aventura_com_bako/features/informacoes_especies/presentation/controller/informacoes_controller.dart';
+import 'package:aventura_com_bako/features/informacoes_especies/presentation/widgets/categoria_especie_widget.dart';
+import 'package:get/get.dart';
 
 class GalleryScreen extends StatefulWidget {
   const GalleryScreen({super.key});
@@ -10,98 +11,106 @@ class GalleryScreen extends StatefulWidget {
 }
 
 class _GalleryScreenState extends State<GalleryScreen> {
+  InformacoesEspeciesController controller =
+      Get.put(InformacoesEspeciesController());
+  final urlImages = [
+    'assets/Paineira-rosa-1.jpg',
+    'assets/Paineira-rosa-2.jpg',
+    'assets/Paineira-rosa-1.jpg',
+    'assets/Paineira-rosa-2.jpg',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Opacity(
-            opacity: 0.5,
-            child: Container(
-              height: double.maxFinite,
-              width: double.maxFinite,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image:
-                      ExactAssetImage('assets/Padrão4.jpg'), //TODO Constantes
-                  fit: BoxFit.fitHeight,
-                ),
+        body: Stack(
+      children: [
+        Opacity(
+          opacity: 0.5,
+          child: Container(
+            height: double.maxFinite,
+            width: double.maxFinite,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: ExactAssetImage('assets/Padrão4.jpg'), //TODO Constantes
+                fit: BoxFit.fitHeight,
               ),
             ),
           ),
-          Align(
-            alignment: Alignment.topLeft,
-            child: Container(
-              margin: const EdgeInsets.only(top: 40),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.3,
-                    height: MediaQuery.of(context).size.height * 0.13,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: ExactAssetImage('assets/bako_vetor.png'),
-                        fit: BoxFit.fitHeight,
-                      ),
+        ),
+        Align(
+          alignment: Alignment.topLeft,
+          child: Container(
+            margin: const EdgeInsets.only(top: 40),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.3,
+                  height: MediaQuery.of(context).size.height * 0.13,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: ExactAssetImage('assets/bako_vetor.png'),
+                      fit: BoxFit.fitHeight,
                     ),
                   ),
-                  Container(
-                    child: Text(
-                      'Galeria',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 45,
-                          overflow: TextOverflow.clip),
+                ),
+                Container(
+                  child: Text(
+                    'Galeria',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 45,
+                        overflow: TextOverflow.clip),
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.3,
+                  height: MediaQuery.of(context).size.height * 0.15,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: ExactAssetImage('assets/bako_vetor.png'),
+                      fit: BoxFit.fitHeight,
                     ),
                   ),
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.3,
-                    height: MediaQuery.of(context).size.height * 0.15,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: ExactAssetImage('assets/bako_vetor.png'),
-                        fit: BoxFit.fitHeight,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
-          Container(
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-              child: GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                  crossAxisSpacing: 5,
-                  mainAxisSpacing: 5,
-                ),
-                itemBuilder: (context, index) {
-                  return RawMaterialButton(
-                    child: InkWell(
-                      child: Ink.image(
-                        image: AssetImage("assets/Paineira-rosa-1.jpg"),
-                        height: 300,
-                        fit: BoxFit.cover,
-                      ),
+        ),
+        Container(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 150),
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                crossAxisSpacing: 5,
+                mainAxisSpacing: 5,
+              ),
+              itemBuilder: (context, index) {
+                return RawMaterialButton(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage(
+                              '${controller.especieLida.value.assets}'),
+                          fit: BoxFit.cover),
                     ),
-                    onPressed: () {
-                      /*Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => GalleryWidget(
-                                        urlImages: urlImages,
-                                        index: index,
-                                      )));*/
-                    },
-                  );
-                },
-                itemCount: 2, //transformedImages.length,
-              ))
-        ],
-      ),
-    );
+                  ),
+                  onPressed: () {
+                    /*Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => GalleryWidget(
+                                            urlImages: urlImages,
+                                            index: index,
+                                          )));*/
+                  },
+                );
+              },
+              itemCount: urlImages.length,
+            )),
+      ],
+    ));
   }
 }
